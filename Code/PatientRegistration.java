c3225d995b2cd0b316f258562d560433c4093dc3
tanguduraviteja <52277919+tanguduraviteja@users.noreply.github.com>
2021-04-20 20:40:58 +0530

    Update PatientRegistration.java

--- Code/PatientRegistration.java
+++ Code/PatientRegistration.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.*;
 public class PatientRegistration {
   public PatientRegistration(String id) {
 	  JFrame f=new JFrame("PatientRegistration");
