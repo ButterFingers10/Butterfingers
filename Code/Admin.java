aeb0ae09aa2a95832e3f6ade26b5b1afd0208075
Mustaq Ahammad <43813438+mustaqmustu@users.noreply.github.com>
2021-04-20 20:43:14 +0530

    Update Admin.java

--- Code/Admin.java
+++ Code/Admin.java
@@ -1,13 +1,7 @@
 import javax.swing.*;
-
-import java.sql.Statement;
 import java.awt.*;
 import java.awt.event.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 public class Admin {
 	static Connection cnct;
 	static PreparedStatement pst;
